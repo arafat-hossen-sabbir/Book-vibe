e01e2621be1eb805206bca27ce439e7d2f4914db
Arafat Hossen Sabbir <ahsabbir476@gmail.com>
2026-04-10 23:48:31 +0600

    Navbar design completed

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -1,82 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
+    <header className="bg-base-100">
+      <div className="navbar container mx-auto px-5">
         <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
-          </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="text-[28px] font-extrabold tracking-wide cursor-pointer">
+            Book Vibe
+          </a>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+
+        <nav className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal gap-4 font-medium">
             <li>
-              <a>Item 1</a>
+              <a className="hover:text-accent transition-colors duration-200">
+                Home
+              </a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a className="hover:text-accent transition-colors duration-200">
+                Listed Books
+              </a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a className="hover:text-accent transition-colors duration-200">
+                Pages to Read
+              </a>
             </li>
           </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        </nav>
+
+        <div className="navbar-end flex items-center gap-3">
+          <a className="btn btn-success">Sign in</a>
+          <a className="btn btn-primary">Sign up</a>
         </div>
       </div>
-    </div>
+    </header>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
